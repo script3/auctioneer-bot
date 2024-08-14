4561f6d66e4e4d150a948f2230fd61be32976c51
Ryang-21 <ryanyang2001@gmail.com>
2024-08-14 16:21:20 -0600

    fix fill block and fill percent calculation

--- src/auction.ts
+++ src/auction.ts
@@ -1,7 +1,8 @@
 import { AuctionData, BackstopToken, Reserve } from '@blend-capital/blend-sdk';
 import { Filler } from './utils/config.js';
 import { SorobanHelper } from './utils/soroban_helper.js';
-
+import { AuctionType } from './utils/db.js';
+import { APP_CONFIG } from './utils/config.js';
 interface FillCalculation {
   fillBlock: number;
   fillPercent: number;
@@ -11,137 +12,113 @@ interface FillCalculation {
  * Calculate the block fill and fill percent for a given auction.
  *
  * @param filler - The filler to calculate the block fill for
+ * @param auctionType - The type of auction to calculate the block fill for
  * @param auctionData - The auction data to calculate the block fill for
- * @param reserves - The reserves to use for the calculation
+ * @param sorobanHelper - The soroban helper to use for the calculation
  */
 export async function calculateBlockFillAndPercent(
   filler: Filler,
+  auctionType: AuctionType,
   auctionData: AuctionData,
-  reserves: Map<string, Reserve>,
   sorobanHelper: SorobanHelper
 ): Promise<FillCalculation> {
-  let totalEffectiveCollateral = 0;
-  let totalEffectiveLiabilities = 0;
-  let cometCollateralValue = 0;
-  let cometLiabilitiesValue = 0;
-  // Sum the effective collateral and liabilities
+  // Represents the effective collateral and liabilities of the auction for health factor calculation
+  let effectiveCollateral = 0;
+  let effectiveLiabilities = 0;
+  // Represents the value of the lot and bid for profit calculation
+  let lotValue = 0;
+  let bidValue = 0;
+  const reserves = (await sorobanHelper.loadPool()).reserves;
+
+  // Sum the effective collateral and lot value
   for (const [assetId, amount] of auctionData.lot) {
-    let reserve = reserves.get(assetId);
+    const reserve = reserves.get(assetId);
     if (reserve !== undefined) {
-      totalEffectiveCollateral += Number(amount * reserve.data.bRate);
-    } else {
-      // If the reserve does not exist, the asset is blend lp tokens
+      if (auctionType !== AuctionType.Interest) {
+        effectiveCollateral += reserve.toEffectiveAssetFromBToken(amount) * reserve.oraclePrice;
+        lotValue += reserve.toAssetFromBToken(amount) * reserve.oraclePrice;
+      }
+      // Interest auctions are in underlying assets
+      else {
+        lotValue += (Number(amount) / 10 ** reserve.config.decimals) * reserve.oraclePrice;
+      }
+    } else if (assetId === APP_CONFIG.backstopTokenAddress) {
       // Simulate singled sided withdraw to USDC
-      let backstopToken = await BackstopToken.load(
-        sorobanHelper.network,
-        sorobanHelper.cometId,
-        (await sorobanHelper.loadPool()).config.blndTkn,
-        sorobanHelper.usdcId
-      );
-      cometCollateralValue +=
-        (await sorobanHelper.simLPTokenToUSDC(Number(amount))) ??
-        (Number(amount) * backstopToken.usdcPerLpToken) / 1e7;
+      let lpTokenValue = await sorobanHelper.simLPTokenToUSDC(Number(amount));
+      if (lpTokenValue !== undefined) {
+        lotValue += lpTokenValue;
+      }
+      // Approximate the value of the comet tokens if simulation fails
+      else {
+        let backstopToken = await sorobanHelper.loadBackstopToken();
+        lotValue += (Number(amount) * backstopToken.lpTokenPrice) / 1e7;
+      }
     }
   }
+  // Sum the effective liabilities and bid value
   for (const [assetId, amount] of auctionData.bid) {
     const reserve = reserves.get(assetId);
     if (reserve !== undefined) {
-      totalEffectiveLiabilities += Number(amount * reserve.data.dRate);
-    } else {
-      // If the reserve does not exist, the asset is blend lp tokens
+      effectiveLiabilities += reserve.toEffectiveAssetFromDToken(amount) * reserve.oraclePrice;
+      bidValue += reserve.toAssetFromDToken(amount) * reserve.oraclePrice;
+    } else if (assetId === APP_CONFIG.backstopTokenAddress) {
       // Simulate singled sided withdraw to USDC
-      let backstopToken = await BackstopToken.load(
-        sorobanHelper.network,
-        sorobanHelper.cometId,
-        (await sorobanHelper.loadPool()).config.blndTkn,
-        sorobanHelper.usdcId
-      );
-      cometCollateralValue +=
-        (await sorobanHelper.simLPTokenToUSDC(Number(amount))) ??
-        (Number(amount) * backstopToken.usdcPerLpToken) / 1e7;
+      let lpTokenValue = await sorobanHelper.simLPTokenToUSDC(Number(amount));
+      if (lpTokenValue !== undefined) {
+        bidValue += lpTokenValue;
+      } else {
+        let backstopToken = await sorobanHelper.loadBackstopToken();
+        bidValue += (Number(amount) * backstopToken.lpTokenPrice) / 1e7;
+      }
     }
   }
-  const totalLotValue = totalEffectiveCollateral + cometCollateralValue;
-  const totalBidValue = totalEffectiveLiabilities + cometLiabilitiesValue;
 
-  // Calculate fillers health factor
-  const pool = await sorobanHelper.loadPool();
-  const fillerState = await sorobanHelper.loadUser(pool, filler.keypair.publicKey());
+  const fillerState = await sorobanHelper.loadUser(filler.keypair.publicKey());
   let fillBlock = 0;
   let fillPercent = 100;
 
-  while (fillBlock < 400) {
-    if (fillBlock <= 200) {
-      let profit = totalLotValue * (fillBlock / 200) - totalBidValue;
+  if (lotValue >= bidValue * (1 + filler.minProfitPct)) {
+    const minLotAmount = bidValue * (1 + filler.minProfitPct);
+    effectiveCollateral = effectiveCollateral * (minLotAmount / lotValue);
+    fillBlock = 200 - (lotValue - minLotAmount) / (lotValue / 200);
+  } else {
+    const maxBidAmount = lotValue * (1 - filler.minProfitPct);
+    effectiveLiabilities = effectiveLiabilities * (maxBidAmount / bidValue);
+    fillBlock = 200 + (bidValue - maxBidAmount) / (bidValue / 200);
+  }
+  fillBlock = Math.min(Math.max(Math.ceil(fillBlock), 0), 400);
 
-      if (profit > filler.minProfitPct * totalBidValue) {
-        // Calculate the new health factor
-        let newHealthFactor =
-          (fillerState.positionEstimates.totalEffectiveCollateral +
-            totalEffectiveCollateral * (fillBlock / 200)) /
-          (fillerState.positionEstimates.totalEffectiveLiabilities + totalEffectiveLiabilities);
-
-        // Adjust the fill percent to maintain the health factor
-        if (newHealthFactor < filler.minHealthFactor) {
-          for (let percent = 99; percent > 0; percent--) {
-            const adjustedFillHealthFactor =
-              (fillerState.positionEstimates.totalEffectiveCollateral +
-                (totalEffectiveCollateral * (fillBlock / 200) * percent) / 100) /
-              (fillerState.positionEstimates.totalEffectiveLiabilities +
-                (totalEffectiveLiabilities * percent) / 100);
-            if (adjustedFillHealthFactor > filler.minHealthFactor) {
-              fillPercent = percent;
-              break;
-            }
-          }
-        }
-        break;
-      }
-      fillBlock++;
-    } else {
-      let profit = totalLotValue - (totalBidValue * (fillBlock - 200)) / 200;
-
-      if (profit > filler.minProfitPct * totalBidValue) {
-        // Calculate the new health factor
-        let newHealthFactor =
-          (fillerState.positionEstimates.totalEffectiveCollateral + totalEffectiveCollateral) /
-          (fillerState.positionEstimates.totalEffectiveLiabilities +
-            (totalEffectiveLiabilities * (fillBlock - 200)) / 200);
-
-        // Adjust the fill percent to maintain the health factor
-        if (newHealthFactor < filler.minHealthFactor) {
-          for (let percent = 99; percent > 0; percent--) {
-            const adjustedFillHealthFactor =
-              (fillerState.positionEstimates.totalEffectiveCollateral +
-                (totalEffectiveCollateral * percent) / 100) /
-              (fillerState.positionEstimates.totalEffectiveLiabilities +
-                (((totalEffectiveLiabilities * (fillBlock - 200)) / 200) * percent) / 100);
-            if (adjustedFillHealthFactor > filler.minHealthFactor) {
-              fillPercent = percent;
-              break;
-            }
-          }
-        }
-
-        // If bid contain comets lp tokens check the balance of fillers comet lp tokens and adjust fill percent
-        if (cometLiabilitiesValue > 0) {
-          const cometLpTokenBalance = await sorobanHelper.getBalance(
-            sorobanHelper.cometId,
-            filler.keypair.publicKey()
-          );
-          if (cometLpTokenBalance < cometLiabilitiesValue) {
-            fillPercent = Math.min(
-              fillPercent,
-              Math.floor((cometLpTokenBalance / cometLiabilitiesValue) * 100)
-            );
-            break;
-          }
-        }
-        break;
-      }
-      fillBlock++;
+  if (effectiveCollateral < effectiveLiabilities) {
+    let additionalLiabilities = effectiveLiabilities - effectiveCollateral;
+    let maxAdditionalLiabilities =
+      (fillerState.positionEstimates.totalEffectiveCollateral -
+        filler.minHealthFactor * fillerState.positionEstimates.totalEffectiveLiabilities) /
+      filler.minHealthFactor;
+    if (additionalLiabilities > maxAdditionalLiabilities) {
+      fillPercent = Math.min(
+        fillPercent,
+        Math.floor((maxAdditionalLiabilities / additionalLiabilities) * 100)
+      );
     }
   }
+
+  // If bid contain comets lp tokens check the balance of fillers comet lp tokens and adjust fill percent
+  if (auctionType === AuctionType.Interest) {
+    const cometLpTokenBalance = await sorobanHelper.simBalance(
+      APP_CONFIG.backstopTokenAddress,
+      filler.keypair.publicKey()
+    );
+    const cometLpBid =
+      fillBlock <= 200
+        ? Number(auctionData.bid.get(APP_CONFIG.backstopTokenAddress)!)
+        : Number(auctionData.bid.get(APP_CONFIG.backstopTokenAddress)!) *
+          (1 - (fillBlock - 200) / 200);
+
+    if (cometLpTokenBalance < cometLpBid) {
+      fillPercent = Math.min(fillPercent, Math.floor((cometLpTokenBalance / cometLpBid) * 100));
+    }
+  }
+
   return { fillBlock, fillPercent };
 }
 
